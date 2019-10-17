649de6681d912359a9c12cd1fc2b932f21679cbb
Daniel Němec <nemecd@cngroup.dk>
2019-10-17 21:38:43 +0200

    Custom color classes added.

--- frontend/src/atoms/Button.js
+++ frontend/src/atoms/Button.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const baseButtonStyles = 'dib bg-animate pv2 ph4 br-pill bn shadow-1';
-
-const primaryColorClasses = 'white bg-green hover-bg-dark-green';
-const secondaryColorClasses = 'white bg-red hover-bg-dark-red';
-const disabledColorClasses = 'black bg-moon-gray';
-
 export const Button = ({
   children,
   className,
@@ -34,3 +28,13 @@ export const Button = ({
     {children}
   </button>
 );
+
+const baseButtonStyles = 'dib bg-animate pv2 ph4 br-pill bn shadow-1 pointer';
+
+const primaryColorClasses =
+  'white bg-dodger-blue hover-dodger-blue hover-bg-white';
+
+const secondaryColorClasses =
+  'spring-green bg-white hover-white hover-bg-spring-green';
+
+const disabledColorClasses = 'black bg-moon-gray';
