e8d3ea682bc5a3367e8997dbb58c2436504e6f91
Daniel Němec <nemec.daniel@seznam.cz>
2019-10-30 21:57:18 +0100

    Formik and yup feature on login page. (#10)

---
+++ frontend/src/organisms/Field.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useField } from 'formik';
+
+import { TextInputWithLabel } from '../molecules';
+
+export const Field = props => {
+  const [field, meta] = useField(props);
+
+  const error = meta.touched && meta.error ? meta.error : undefined;
+
+  return <TextInputWithLabel error={error} {...field} {...props} />;
+};
--- frontend/src/organisms/index.js
+++ frontend/src/organisms/index.js
@@ -1,3 +1,4 @@
+export { Field } from './Field';
 export { LoggedInHeader } from './LoggedInHeader';
 export { NotLoggedInHeader } from './NotLoggedInHeader';
 export { NewLobbyForm } from './newLobbyForm';
