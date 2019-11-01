7f2ba3010dcb912e9945a97a4044865736cdb227
Martin Krivda <martin.krivda@kobchocen.cz>
2019-11-01 21:52:14 +0100

    WHERE clause in event detail

--- backend/src/modules/event/eventRoutes.js
+++ backend/src/modules/event/eventRoutes.js
@@ -14,14 +14,15 @@ router.get('/list', async (req, res, next) => {
 
 router.get('/:eventId', async (req, res, next) => {
   const { eventId } = req.params;
+  const { userId } = req.jwtDecoded;
   const dbConnection = req[DB_CONNECTION_KEY];
   const eventDetail = await dbConnection.query(
     'SELECT game_id, country, sport_id, name, date_from, date_to, location, description, min_users, max_users FROM game WHERE active = true AND game_id = ?;', 
     [eventId],
   );
   const dbResponse = await dbConnection.query(
-    'SELECT lobby.lobby_id, lobby.name, game.name as eventName, leader_id, private, max_players, min_users, max_users, count(lobby_user.user_id) as joinedUsers FROM lobby LEFT JOIN game ON game.game_id = lobby.game_id LEFT JOIN lobby_user ON lobby.lobby_id = lobby_user.lobby_id WHERE lobby.game_id = ? AND lobby.active = true GROUP BY lobby.lobby_id;', 
-    [eventId],
+    'SELECT lobby.lobby_id, lobby.name, game.name as eventName, leader_id, private, max_players, min_users, max_users, count(lobby_user.user_id) as joinedUsers FROM lobby LEFT JOIN game ON game.game_id = lobby.game_id LEFT JOIN lobby_user ON lobby.lobby_id = lobby_user.lobby_id WHERE lobby.game_id = ? AND lobby_user.user_id = ? AND lobby.active = true GROUP BY lobby.lobby_id;', 
+    [eventId, userId],
   );
   const lobbyList = dbResponse.map(({lobby_id: id, min_users: minimumUsers, max_players: maxUsers, ...rest}) => ({id, minimumUsers, maxUsers, ...rest}));
 
