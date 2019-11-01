e194329e2bebdd4622489b5e05f29d3b5ed06669
Daniel Němec <nemecd@cngroup.dk>
2019-11-01 14:53:02 +0100

    Hack...

--- backend/src/modules/auth/authRoutes.js
+++ backend/src/modules/auth/authRoutes.js
@@ -90,8 +90,8 @@ router.post(
         .json({ error: '422: This email is already registered' });
     }
 
-    bcrypt.hash(password, 10, async (err, hash) => {
-      if (!err) {
+    bcrypt.hash(password, 10, async (error, hash) => {
+      if (!error) {
         const dbResponse = await dbConnection.query(
           `INSERT INTO users (user_id, email, password, active) 
       VALUES (NULL, ?, ?, ?);`,
