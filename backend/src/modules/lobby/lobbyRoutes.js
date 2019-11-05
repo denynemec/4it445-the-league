9464fad010dc1298da009ab9922dde797a4f74a7
BuildTools <unconfigured@null.spigotmc.org>
2019-11-05 18:43:06 +0100

    FetchDraftBackend

--- backend/src/modules/lobby/lobbyRoutes.js
+++ backend/src/modules/lobby/lobbyRoutes.js
@@ -16,8 +16,34 @@ router.get('/:lobbyId', (req, res, next) => {
   res.json({});
 });
 
-router.get('/:lobbyId/fetchDraft', (req, res, next) => {
-  res.json({ draft: 1 });
+router.get('/:lobbyId/fetchDraft', async (req, res, next) => {
+  const { lobbyId } = req.params;
+
+  const dbConnection = req[DB_CONNECTION_KEY];
+
+  const dbResponseNumber = await dbConnection.query(
+    `SELECT COUNT(user_id) AS playersCount FROM lobby_user WHERE lobby_id = '${lobbyId}';`,
+  );
+
+  const { playersCount: playersCount } = dbResponseNumber[0];
+
+  for (var i = 0; i < playersCount; i++) {
+    const dbResponse = await dbConnection.query(
+      `UPDATE lobby_user SET draft_order='${i +
+        1}' WHERE draft_order IS NULL and lobby_id = '${lobbyId}' ORDER BY RAND() LIMIT 1;`,
+    );
+    if (dbResponse.affectedRows === 0) {
+      return res
+        .status(409)
+        .json({ error: '409: Draft order already generated' });
+    }
+  }
+
+  const dbResponse = await dbConnection.query(
+    `SELECT users.nickname, lobby_user.user_id, lobby_user.draft, lobby_user.draft_order FROM lobby_user JOIN users ON lobby_user.user_id = users.user_id WHERE lobby_id = '${lobbyId}' ORDER BY lobby_user.draft_order;`,
+  );
+
+  res.json(dbResponse);
 });
 
 export default router;
--- frontend/src/pages/draftDetail/DraftDetail.js
+++ frontend/src/pages/draftDetail/DraftDetail.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-// import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 import ENDPOINTS from '../../endpoints';
-import { LoadingSpinner } from '../../atoms';
+import { LoadingSpinner, Heading } from '../../atoms';
 import { LoggedInPageLayout } from '../../templates';
 import { useFetchRequest } from '../../utils';
 
 export const DraftDetail = () => {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
   const { lobbyId } = useParams();
 
   const fetchDraftState = useFetchRequest(ENDPOINTS.fetchDraft(lobbyId));
-
-  console.log(fetchDraftState);
 
   return (
     <LoggedInPageLayout errorList={[{ id: 1, error: fetchDraftState.error }]}>
       {fetchDraftState.isLoading && <LoadingSpinner />}
 
-      {fetchDraftState.data && 'test'}
+      <Heading className="flex justify-center pb2">
+        {t('Page.Draft.DraftOrderHeading')}
+      </Heading>
     </LoggedInPageLayout>
   );
 };
