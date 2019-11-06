39cb56ff73100affce112b93499b05e56bd9da6d
BuildTools <unconfigured@null.spigotmc.org>
2019-11-06 17:33:20 +0100

    SQL Prepared statements, DraftOrder improvements

--- backend/src/modules/lobby/lobbyRoutes.js
+++ backend/src/modules/lobby/lobbyRoutes.js
@@ -10,7 +10,14 @@ router.get('/list', async (req, res, next) => {
     'SELECT lobby.lobby_id, lobby.name, game.name as eventName, leader_id, private, max_players, min_users, max_users, count(lobby_user.user_id) as joinedUsers FROM lobby LEFT JOIN game ON game.game_id = lobby.game_id LEFT JOIN lobby_user ON lobby.lobby_id = lobby_user.lobby_id WHERE lobby_user.user_id = ? AND lobby.active = true GROUP BY lobby.lobby_id;',
     [userId],
   );
-  const lobbyList = dbResponse.map(({lobby_id: id, min_users: minimumUsers, max_players: maxUsers, ...rest}) => ({id, minimumUsers, maxUsers, ...rest}));
+  const lobbyList = dbResponse.map(
+    ({
+      lobby_id: id,
+      min_users: minimumUsers,
+      max_players: maxUsers,
+      ...rest
+    }) => ({ id, minimumUsers, maxUsers, ...rest }),
+  );
   res.json(lobbyList);
 });
 
@@ -28,15 +35,22 @@ router.get('/:lobbyId/fetchDraft', async (req, res, next) => {
   const dbConnection = req[DB_CONNECTION_KEY];
 
   const dbResponseNumber = await dbConnection.query(
-    `SELECT COUNT(user_id) AS playersCount FROM lobby_user WHERE lobby_id = '${lobbyId}';`,
+    `SELECT COUNT(user_id) AS playersCount FROM lobby_user WHERE lobby_id = ?;`,
+    [lobbyId],
   );
 
   const { playersCount: playersCount } = dbResponseNumber[0];
 
+  const dbResponsePlayers = await dbConnection.query(
+    `SELECT user_id FROM lobby_user WHERE lobby_id = ? ORDER BY RAND();`,
+    [lobbyId],
+  );
+
   for (var i = 0; i < playersCount; i++) {
+    const { user_id: user_id } = dbResponsePlayers[i];
     const dbResponse = await dbConnection.query(
-      `UPDATE lobby_user SET draft_order='${i +
-        1}' WHERE draft_order IS NULL and lobby_id = '${lobbyId}' ORDER BY RAND() LIMIT 1;`,
+      `UPDATE lobby_user SET draft_order= ? WHERE draft_order IS NULL and lobby_id = ? AND user_id = ? LIMIT 1;`,
+      [i + 1, lobbyId, user_id],
     );
     if (dbResponse.affectedRows === 0) {
       return res
@@ -46,7 +60,8 @@ router.get('/:lobbyId/fetchDraft', async (req, res, next) => {
   }
 
   const dbResponse = await dbConnection.query(
-    `SELECT users.nickname, lobby_user.user_id, lobby_user.draft, lobby_user.draft_order FROM lobby_user JOIN users ON lobby_user.user_id = users.user_id WHERE lobby_id = '${lobbyId}' ORDER BY lobby_user.draft_order;`,
+    `SELECT users.nickname, lobby_user.user_id, lobby_user.draft, lobby_user.draft_order FROM lobby_user JOIN users ON lobby_user.user_id = users.user_id WHERE lobby_id = ? ORDER BY lobby_user.draft_order;`,
+    [lobbyId],
   );
 
   res.json(dbResponse);
