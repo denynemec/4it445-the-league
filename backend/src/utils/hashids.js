0ed79c1257e1253de39a52850f0630fb7b3a9adf
Daniel Němec <nemecd@cngroup.dk>
2019-11-13 12:37:05 +0100

    Secret key variables move to env file.

--- backend/src/utils/hashids.js
+++ backend/src/utils/hashids.js
@@ -1,7 +1,7 @@
 import Hashids from 'hashids/cjs';
 
-const HASHIDS_SECRET_KEY = 'HASHIDS_SECRET_KEY';
-const HASHIDS_PADDING = 15;
+const HASHIDS_SECRET_KEY = process.env.HASHIDS_SECRET_KEY;
+const HASHIDS_PADDING = process.env.HASHIDS_PADDING;
 
 const hashids = new Hashids(HASHIDS_SECRET_KEY, HASHIDS_PADDING);
 
--- backend/src/utils/jwtToken.js
+++ backend/src/utils/jwtToken.js
@@ -1,9 +1,9 @@
 import jwt from 'jsonwebtoken';
 
-const JWT_TOKEN_SECRET = 'JWT_TOKEN_SECRET';
+const JWT_TOKEN_SECRET_KEY = process.env.JWT_TOKEN_SECRET_KEY;
 
 // generate JWT token with no expiration
-export const getJwtToken = payload => jwt.sign(payload, JWT_TOKEN_SECRET);
+export const getJwtToken = payload => jwt.sign(payload, JWT_TOKEN_SECRET_KEY);
 
 // verify JWT token and save decoded payload into req.jwtDecoded
 export const verifyJwtToken = (req, res, next) => {
@@ -14,7 +14,7 @@ export const verifyJwtToken = (req, res, next) => {
     const token = tokenWithBearer.slice(7, tokenWithBearer.length);
 
     try {
-      req.jwtDecoded = jwt.verify(token, JWT_TOKEN_SECRET);
+      req.jwtDecoded = jwt.verify(token, JWT_TOKEN_SECRET_KEY);
       next();
     } catch (error) {
       return res.status(401).json({ error: '401: Unauthorized' });
