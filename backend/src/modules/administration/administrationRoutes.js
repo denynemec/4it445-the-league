b024b3fa990e8355ac5e8483a3a97a9c13486781
martinkrivda <martin.krivda@kobchocen.cz>
2019-11-18 21:57:35 +0100

    Feature/upload csv database (#39)
    * Import players and teams to db
    
    * Check files in request
    
    * Formatting 2
    
    * Check for uploading file 14 days before event
    
    * Delete players from event before upload
    
    * Prettier format 3
    
    * Missing semicolon
    
    * Code review changes

--- backend/src/modules/administration/administrationRoutes.js
+++ backend/src/modules/administration/administrationRoutes.js
@@ -21,9 +21,42 @@ router.post(
       });
     }
 
+    //if (!req.files) {
+    //  return res.send({ error: 'CSV file was not found' });
+    //}
+
+    //const file = req.files.eventPlayers;
+
     const csv = require('csv-parser');
     const fs = require('fs');
     const dbConnection = req[DB_CONNECTION_KEY];
+    const { eventId } = req.body;
+
+    const eventResponse = await dbConnection.query(
+      'SELECT game_id, date_from FROM game WHERE game_id = ? AND active = true;',
+      [eventId],
+    );
+
+    if (!eventResponse || eventResponse.length < 1) {
+      return res.status(422).json({ error: 'No event for you' });
+    }
+    const today = new Date();
+    const endUpload = eventResponse[0].date_from;
+    endUpload.setDate(endUpload.getDate() - 14);
+    if (today > endUpload) {
+      return res.status(422).json({
+        error: 'It is too late. You can not upload players to this event',
+      });
+    }
+    await dbConnection.query('DELETE FROM player_game WHERE game_id = ?;', [
+      eventId,
+    ]);
+
+    const dbPlayer = await dbConnection.query(
+      'SELECT player_id, firstname, lastname FROM player;',
+    );
+
+    const dbTeam = await dbConnection.query('SELECT team_id, name FROM team;');
 
     fs.createReadStream('./src/modules/administration/player.csv')
       .pipe(
@@ -33,23 +66,40 @@ router.post(
         }),
       )
       .on('data', async player => {
-        console.log(player);
-        const playerExist = await dbConnection.query(
-          'SELECT firstname, lastname FROM player WHERE firstname = ? AND lastname = ? LIMIT 1',
-          [player[0], player[1]],
+        let teamId;
+        let playerId;
+        const teamExist = dbTeam.find(t => t.name == player[4]);
+        if (teamExist === 'undefined') {
+          const dbTeamResponse = await dbConnection.query(
+            'INSERT INTO team (name, created_at, updated_at) VALUES (?, NOW(), NOW());',
+            [player[4]],
+          );
+          teamId = dbTeamResponse.insertId;
+        } else {
+          teamId = teamExist.team_id;
+        }
+        const playerExist = dbPlayer.find(
+          pl => pl.firstname == player[0] && pl.lastname == player[1],
         );
-        if (playerExist.length < 1) {
+        if (playerExist === 'undefined') {
           const dbPlayerResponse = await dbConnection.query(
             'INSERT INTO player (firstname, lastname, created_at, updated_at) VALUES (?, ?, NOW(), NOW());',
             [player[0], player[1]],
           );
+          playerId = dbPlayerResponse.insertId;
+        } else {
+          playerId = playerExist.player_id;
         }
+        await dbConnection.query(
+          'INSERT INTO player_game (game_id, player_id, post_abbr, team_id, number) VALUES (?, ?, ?, ?, ?);',
+          [eventId, playerId, player[3], teamId, player[2]],
+        );
       })
       .on('end', () => {
         console.log('CSV file successfully processed');
       });
 
-    res.json({});
+    res.json({ message: 'CSV file successfully processed' });
   },
 );
 export default router;
