335298398225b46d951defc3d4af416b396ea9ef
martinkrivda <martin.krivda@kobchocen.cz>
2019-12-08 19:23:02 +0100

    React strap form feedback a input validation (#56)

--- frontend/src/molecules/TextInputWithLabel.js
+++ frontend/src/molecules/TextInputWithLabel.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
-import { Error, Label } from '../atoms';
-import { FormGroup, Input } from 'reactstrap';
+import { FormGroup, Label, Input, FormFeedback} from 'reactstrap';
 
 export function TextInputWithLabel({ error, label, inputRef, ...inputProps }) {
   return (
     <FormGroup>
       <Label className="flex flex-column ph2 pv2 fw6 w-100 border-box">
         {label}
-        <Input ref={inputRef} {...inputProps} />
-        {error && <Error>{error}</Error>}
+        <Input invalid={error ? true : false} ref={inputRef} {...inputProps} />
+        {error && <FormFeedback>{error}</FormFeedback>}
       </Label>
     </FormGroup>
   );
