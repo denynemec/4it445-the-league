2a06cfddf34b44dcdaa26bcbb1feb71965f6395d
Martin Krivda <martin.krivda@kobchocen.cz>
2019-12-11 12:48:34 +0100

    Draft date

--- backend/src/modules/lobby/lobbyRoutes.js
+++ backend/src/modules/lobby/lobbyRoutes.js
@@ -65,13 +65,12 @@ router.post(
           error: '422: Emails are not in interval <minEmails, maxEmails>',
         });
       }
-      /*
-      if (draftStartTime < Date.now) {
+
+      if (Date.parse(draftStartTime) < Date.now()) {
         return res.status(422).json({
           error: 'Draft time must be in the future',
         });
       }
-      */
     } else {
       return res.status(422).json({
         error: '422: The game was cancelled.',
